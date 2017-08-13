6ca9961d5717e3f6d3c238b438f693cc970cb74c
Ana <ana.toma92@gmail.com>
2017-08-13 14:48:41 +0300

    add full functionality

--- server.js
+++ server.js
@@ -3,11 +3,10 @@
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
-var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-var env = require('node-env-file');
-var dns = require('dns');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var env = require('node-env-file');
 
 // Reads from .env file
 env(__dirname + '/.env');
@@ -36,80 +35,63 @@ var LinkInfo = require('./models/links.js');
 app.use(bodyParser.json());// to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended:true}));// to support URL-encoded bodies
 
-app.post('/api/shorturl/new', function(req,res){
-  var oldUrl = req.body.url;
-  var newUrl = Date.now()%100000;
-  //console.log(newUrl);
-
-  // Modify address
-  if(oldUrl.indexOf('http://')!==-1){
-    oldUrl = oldUrl.split("").splice(7).join("");
-    //console.log(oldUrl);
-  }
-  if(oldUrl.indexOf('https://')!==-1){
-    oldUrl = oldUrl.split("").splice(8).join("");
-  }
-  if(oldUrl.indexOf('/')!==-1){
-    oldUrl = oldUrl.split("").splice(0,oldUrl.indexOf('/')).join("");
-  }
-
-  //Check if address is valid
-  //console.log(oldUrl);
-  dns.lookup(oldUrl, function(err, address){
-    if(err) return res.json({error: "invalid URL"});
-
-    var addLink = new LinkInfo({fullLink: oldUrl, shortLink:newUrl});
-
-    //check for oldUrl duplicates
-    LinkInfo.find({"fullLink": oldUrl}, function(err,link){
-      if(err) throw err;
-        //console.log('this is link'+link);
-      if(link.oldUrl !== oldUrl){//if the link does not exist in database
-        //because the database is so small and the probability of generating shortLink duplicates
-        //is almost 0, I am only checking for fullLink duplicates
-        addLink.save();
-        //console.log("Old url is: " +  oldUrl);
-        //console.log("New url is: " +  newUrl);
-        //console.log("addLink: " +  addLink);
-
-        return res.json({
-          oldURL: oldUrl, 
-          newURL:newUrl
-        });
-      }
-    });
-    //console.log('link alone is: ' + link);
-    //
-  })
-
-});
-
-app.get('/:code', function(req,res){
-  //console.log(req.params);
-  var code = req.params.code.toString();
-  LinkInfo.findOne({"shortLink":code}, function(err,result){
-    console.log("result is"+result);
-    if (err) throw err;
-    if(result===null) {
-      res.json({error: "link does not exist in database"});
-    } else {
-      res.redirect("www.google.com");
-    }
-    
-
-  })
-});
-
+// Display Homepage
 app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint... 
-app.get("/api/new", function (req, res) {
-  res.json({oldURL: "old stuff", newURL:"new stuff"});
+// API
+app.post('/api/shorturl/new', function(req,res){
+  var oldUrl = req.body.url;
+  var newUrl = Date.now()%100000;
+
+  // Regex from https://gist.github.com/dperini/729294
+  var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+  if (regex.test(oldUrl)){
+
+    var addLink = new LinkInfo({fullLink: oldUrl, shortLink:newUrl});
+
+    //check database for duplicates
+    //!!! due to a very small db, it is very unlikely there will be newUrl duplicates
+    LinkInfo.findOne({"fullLink": oldUrl}, function(err,link){
+      if(err) throw err;
+      if(link===null){//if there are no duplicates
+          addLink.save(); // add object to database
+          return res.json({ // return object
+          original_url: oldUrl, 
+          short_url:newUrl
+        })
+      } else {//if there are duplicates
+        return res.json({
+          original_url: oldUrl, 
+          short_url: link.shortLink
+        })
+      }
+    });
+  } else {
+      return res.json({
+        "error":"invalid URL"
+      })
+    }
 });
 
+// Retrieve link from database and redirect to it
+app.get('/:code', function(req,res){
+
+  var code = req.params.code.toString();
+
+  LinkInfo.findOne({"shortLink":code}, function(err,result){
+
+    if (err) throw err;
+
+    if(result===null) {
+      res.json({error: "link does not exist in database"});
+    } else {
+      res.redirect(result.fullLink);
+    }
+  })
+});
 
 // Connect to port 3000
 var port = process.env.PORT || 3000;
